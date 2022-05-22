b56f82d1740a7fd5f67e5b4cdccc77963c0bbab6
Tumisang Fokase <62010042+TumisangF@users.noreply.github.com>
2022-05-22 12:03:55 +0200

    Update 290 Word Pattern.py

--- 290 Word Pattern.py
+++ 290 Word Pattern.py
@@ -4,28 +4,32 @@
 # e.g pattern = 'abba' ,string = 'dog cat cat dog'
 
 def wordPattern(pattern,string)
-  # mapPS char --> word
-  # mapSP word --> char
-  mapPS,mapSP = {},{}
-  
-  # create a a list of words from the string
-  list_words = string.split()
-  
-  # The lengths should be the same
-  if len(pattern) != len(string):
-    return False
-  
-  for i in range(len(pattern)):
-    char,word = pattern[i],list_words[i]
+	# edge case: if all words in str are the same. creating two dictionaries would solve that.
+	# 'abba' and s = 'dog dog dog dog'
+	# first iter: a --> dog, dog --> a
+	# sec   iter: b --> dog, dog --> b, it would retunr false since same word can't be mapped to two diff characters
+        	
+	mapCharWord,mapWordChar = {},{}
+        
+	# create a list of words from the str
+	list_words = s.split()
+        
+	# Lengths have to be the same
+	if len(pattern) != len(list_words):
+		return False
+        
+	for i in range(len(pattern)):
+		char,word= pattern[i],list_words[i]
+          
+	# check if there is a occurance of char and word
+	if (char in mapCharWord and mapCharWord[char] != word) or (word in mapWordChar and mapWordChar[word] != char):
+		return False
+            
+		mapCharWord[char] = word
+		mapWordChar[word] = char
+       
+	# Everything checks out
+	return True
     
-    #if there is a mapping already of char/word
-    if (char in mapPS and mapPS[char] != word) or (word in mapSP and maSP[word] != char):
-      return False
-    
-    mapPS[char] = word
-    mapSO[word] = word
-    
-  # if everything checks out
-  return True
-    
+	
   
